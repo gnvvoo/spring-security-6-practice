0f8b8cedd8f64cffa948841aa38726e7c746bd46
gnvvoo <t412@naver.com>
2026-06-15 14:25:13 +0900

    feat: JwtProvider 구현

---
+++ src/main/java/com/example/jwt/JwtProvider.java
@@ -0,0 +1,44 @@
+package com.example.jwt;
+
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.JwtException;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.security.Keys;
+import org.springframework.stereotype.Component;
+
+import javax.crypto.SecretKey;
+import javax.xml.crypto.Data;
+import java.nio.charset.StandardCharsets;
+import java.util.Date;
+
+@Component
+public class JwtProvider {
+    private final SecretKey SECRET_KEY = Keys.hmacShaKeyFor("spring-security-6".getBytes(StandardCharsets.UTF_8));
+    private final long EXPIRATION_TIME = 1000L * 60 * 60;
+
+    public String generateToken(String email) {
+        Date now = new Date();
+        Date exp = new Date(now.getTime() + EXPIRATION_TIME);
+
+        return Jwts.builder()
+                .subject(email)
+                .issuedAt(now)
+                .expiration(exp)
+                .signWith(SECRET_KEY)
+                .compact();
+    }
+
+    public boolean validateToken(String token) {
+        try {
+            Jwts.parser()
+                    .verifyWith(SECRET_KEY)
+                    .build()
+                    .parseSignedClaims(token);
+
+            return true;
+        } catch (JwtException | IllegalArgumentException e) {
+            return false;
+        }
+
+    }
+}
